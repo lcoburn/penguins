24484f8e32427783bade5ad3be8fa97605aa4a09
lcoburn <lukecoburn@gmail.com>
2018-01-10 17:48:16 +0000

    The update of pos is now working. It had to be called a global variable in the loop for some unknown reason.

--- penguins1.py
+++ penguins1.py
@@ -11,8 +11,34 @@ import matplotlib.animation as animation
 from operator import add
 
 
-def animate(t, n, r, L, noise, dt, v0, a, f, tt, k, tau, s, tau1, pos, R, F, S):
-    
+n = 100;          # number of penguin speed
+r = 0.5;         # radius of penguin
+temp_init = 1;   # temperture of penguin
+T0 = 1;
+
+L = 20.0;         # domain size
+noise = 0.01;   # noise amplitude
+dt = 0.1;       # time step size
+
+v0 = 0.1;       # penguin speed
+a = 1;          # weighting of prior orientation on heading
+f = 10000;         # weighting of soft repulsion on orientation
+tt = 0.1;        # weighting of temperature gradient on orientation
+k = 0.5;        # temp decay
+tau = 0.01;     # relaxation constant for angle
+s = 1;
+tau1 = 0.1;     # temperature relaxation
+
+
+pos = np.array(2*L*np.random.random((n,2)) - 0.5*L)
+
+R = np.zeros([n,n])      # array for penguin penguin distance;
+F = np.zeros([n,2])     # orrientation vector of penguins
+S = np.zeros([n,2])            # visual for each penguin to follow
+
+
+def animate(t, n, r, L, noise, dt, v0, a, f, tt, k, tau, s, tau1, R, F, S):
+    global pos 
     # penguin-penguin distance
     for i in range(1,n):
         R[:, i] = np.sqrt(np.square(pos[:, 0] - pos[i, 0]) + np.square(pos[:, 1] - pos[i, 1]))
@@ -61,38 +87,11 @@ def animate(t, n, r, L, noise, dt, v0, a, f, tt, k, tau, s, tau1, pos, R, F, S):
 
     print pos[0,0], pos[0,1]
     print '***'
-
+    plt.cla()
     ax.plot(pos[:,0], pos[:,1], 'ok', markersize= 11)
     plt.title(t)
-    print
     
-    return ax,pos
-
-
-
-
-n = 100;          # number of penguin speed
-r = 0.5;         # radius of penguin
-temp_init = 1;   # temperture of penguin
-T0 = 1;
-
-L = 20;         # domain size
-noise = 0.01;   # noise amplitude
-dt = 0.1;       # time step size
-
-v0 = 0.1;       # penguin speed
-a = 1;          # weighting of prior orientation on heading
-f = 10000;         # weighting of soft repulsion on orientation
-tt = 0.1;        # weighting of temperature gradient on orientation
-k = 0.5;        # temp decay
-tau = 0.01;     # relaxation constant for angle
-s = 1;
-tau1 = 0.1;     # temperature relaxation
-
-pos = np.array(2*L*np.random.random((n,2)) - 0.5*L)
-R = np.zeros([n,n])      # array for penguin penguin distance;
-F = np.zeros([n,2])     # orrientation vector of penguins
-S = np.zeros([n,2])            # visual for each penguin to follow
+    return ax
 
 
 fig = plt.figure()
@@ -101,6 +100,10 @@ ax = fig.add_subplot(1, 1, 1)
 ax.set_xlim(0, L)
 ax.set_ylim(0, L)
 plt.title('time = 0')
+
+#for t in range(10):
+    #animate(t, n, r, L, noise, dt, v0, a, f, tt, k, tau, s, tau1, R, F, S)
+    
 #class matplotlib.animation.FuncAnimation(fig, func, frames=None, init_func=None, fargs=None, save_count=None, **kwargs)
-anim = animation.FuncAnimation(fig, animate,fargs=(n, r, L, noise, dt, v0, a, f, tt, k, tau, s, tau1, pos, R, F, S))
+anim = animation.FuncAnimation(fig, animate, fargs=(n, r, L, noise, dt, v0, a, f, tt, k, tau, s, tau1, R, F, S))
 plt.show()
